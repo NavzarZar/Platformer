1b1c55ce767432ba3504971a008dc82c02fe15a5
NavzarZar <razvan.craciun@cognitive.ro>
2023-10-14 18:22:42 +0200

    Commented out frame display and other unecessary code

--- src/main/java/game/GamePanel.java
+++ src/main/java/game/GamePanel.java
@@ -8,10 +8,10 @@ import java.awt.Graphics;
 public class GamePanel extends JPanel{
     //Coordinates where rectangle is drawn at
     private int xDelta = 100, yDelta = 100;
-    Map map = new Map();
-    private int frames = 0;
-    private long lastChecked = 0;
-    //Constructor
+    Map map;
+    //    private int frames = 0;
+    //    private long lastChecked = 0;
+
     public GamePanel() {
         MouseInputs mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
@@ -22,7 +22,6 @@ public class GamePanel extends JPanel{
 
     public void changeX(int value){
         this.xDelta += value;
-
     }
 
     public void changeY(int value){
@@ -51,14 +50,13 @@ public class GamePanel extends JPanel{
         //Draw functions
         g.fillRect(xDelta,yDelta,50, 50);
 
-
-        frames++;
-        //Check if a second passes/ has passed.
-        if(System.currentTimeMillis() - lastChecked >= 1000) {
-            lastChecked = System.currentTimeMillis();
-            System.out.println("FPS: " + frames);
-            frames = 0;
-        }
+//        Display framerate
+//        frames++;
+//        if(System.currentTimeMillis() - lastChecked >= 1000) {
+//            lastChecked = System.currentTimeMillis();
+//            System.out.println("FPS: " + frames);
+//            frames = 0;
+//        }
     }
 
     public void displayMap() {
--- src/main/java/inputs/KeyboardInputs.java
+++ src/main/java/inputs/KeyboardInputs.java
@@ -20,7 +20,7 @@ public class KeyboardInputs implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
 
-        int delta = 5;
+        int delta = 10;
 
         //Searches for keyboard input, changes the pos where the rectangle is drawn (X,Y) in regards with said input
         switch (e.getKeyCode()) {
